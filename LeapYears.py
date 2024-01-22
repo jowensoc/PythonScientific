42b31e70ed28e61a524895f6fcf8e511afdcbcaa
John Owens <john.owens@opencastsoftware.com>
2024-01-22 09:17:00 +0000

    Adding original files

---
+++ LeapYears.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 10:45:59 2024
+
+@author: john.owens
+"""
+
+def isLeapYear(val): 
+    
+    
+
+for i in range(-5, 6):
+    l.append(i + 1)
---
+++ Task2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 18 12:20:57 2024
+
+@author: john.owens
+"""
+
+l = []
+
+for i in range(-5, 6):
+    l.append(i + 1)
+    
+print(l)
