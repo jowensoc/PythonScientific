80f240db52a42f4267ecb7d9e328365591610b2a
John Owens <john.owens@opencastsoftware.com>
2024-01-22 14:54:42 +0000

    Updated Leap Years

--- LeapYears.py
+++ LeapYears.py
@@ -6,9 +6,22 @@ Created on Fri Jan 19 10:45:59 2024
 @author: john.owens
 """
 
-def isLeapYear(val): 
+l = [2020, 2096, 2100, 2104, 2300, 2400]
+
+def isLeapYear(val):
+    leapYearFlag = False
+    if val % 400 == 0:
+        leapYearFlag = True
+    elif val % 100 == 0:
+        leapYearFlag = False
+    else:
+        leapYearFlag =  val % 4 == 0
+        
+    filler = (" NOT ", " ")[leapYearFlag]
+    
+    print("{0} is{1}a Leap Year".format( val, filler))
     
     
 
-for i in range(-5, 6):
-    l.append(i + 1)
+for i in l:
+    isLeapYear(i)
